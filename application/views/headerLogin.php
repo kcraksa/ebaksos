0885ab782daf829d59385dbef48db39d3d5a3a5e
kcraksa <kciptaraksa@gmail.com>
2017-10-10 18:13:18 +0700

    Add files via upload

---
+++ application/views/headerLogin.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>e-Baksos - Selalu Ada Cara Untuk Menolong Sesama</title>
+	<!-- CSS -->
+	<link rel="stylesheet" href="<?php echo base_url() ?>assets/css/bootstrap.min.css">
+	<!-- <link rel="stylesheet" href="<?php echo base_url() ?>assets/css/style.css"> -->
+	<link rel="stylesheet" href="<?= base_url() ?>assets/css/font-awesome.min.css">
+
+	<!-- Javascript -->
+	<script type="text/Javascript" src="<?=base_url().'assets/js/jquery.min.js'?>"></script>
+	<script type="text/Javascript" src="<?=base_url().'assets/js/bootstrap.min.js'?>"></script>
+</head>
+<body>
+
+	<!-- Header -->
+
+	<div id="header">
+		<nav class="navbar navbar-inverse">
+			<div class="container-fluid">
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="<?= base_url() ?>index.php/welcome/showRegisterForm"><i class="fa fa-id-card-o"></i> Daftar Sebagai Penyelenggara</a></li>
+					<li><a href="#"><i class="fa fa-users"></i> Daftar Sebagai Peserta</a></li>
+					<li><a href="#"><i class="fa fa-sign-in"></i> Login</a></li>
+				</ul>
+			</div>
+		</nav>	
+	</div>
+
+	<!-- End Header -->
